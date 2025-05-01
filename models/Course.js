2dd2bced49402fe75b2fab8244ddb0d7a0027e55
esty3439 <esty0548535141@gmail.com>
2025-05-01 19:38:02 +0300

    add user reference to course model

--- models/Course.js
+++ models/Course.js
@@ -22,6 +22,11 @@ const CourseSchema=new mongoose.Schema(
             type:[mongoose.Schema.Types.ObjectId],
             required:true,
             ref:"Challenge" 
+        },
+        user:{
+            type:mongoose.Schema.Types.ObjectId,
+            required:true,
+            ref:"User" 
         }
     },
     {
