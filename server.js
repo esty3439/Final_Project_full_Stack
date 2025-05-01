8ff74e12aeebab1b63b2d9b149ffc62257de5679
esty3439 <esty0548535141@gmail.com>
2025-05-01 19:42:15 +0300

    update server js

--- server.js
+++ server.js
@@ -15,7 +15,6 @@ app.use(express.json())
 
 app.use('/api/auth',require('./routes/authRoutes'))
 app.use('/api/users',require('./routes/userRoutes'))
-app.use('/api/words',require('./routes/wordRoutes'))
 
 connectDB()
 
