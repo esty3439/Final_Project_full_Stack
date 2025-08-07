6e91974fac1cae2a5356b9f50117502d3481787a
esty3439 <esty0548535141@gmail.com>
2025-08-07 15:17:46 +0300

    add sound in word card

---
+++ frontend/src/utils/speech.js
@@ -0,0 +1,20 @@
+const speak = (word) => {
+
+    if (!word)
+        return
+
+    if (!('speechSynthesis' in window)) {
+        console.log('Speech synthesis is not supported in this browser.');
+        return;
+    }
+
+
+    const utterance = new SpeechSynthesisUtterance(word)
+
+    utterance.lang = 'en-US'
+    utterance.rate = 0.7
+
+    speechSynthesis.speak(utterance)
+}
+
+export default speak
