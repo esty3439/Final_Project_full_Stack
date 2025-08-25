9c5264f2b7d1777bcb509e69bb304ececda3aa87
esty3439 <esty0548535141@gmail.com>
2025-08-25 18:22:54 +0300

    add favorite word in backend

---
+++ backend/routes/favoriteWordRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const router = express.Router()
+const favoriteWordController = require('../controllers/favoriteWordController')
+const verifyJWT = require('../middleware/verifyJWT')
+
+//use middleware
+router.use(verifyJWT)
+
+router.get('/',favoriteWordController.getAllFavoriteWords)
+router.get('/:id',favoriteWordController.getSingleFavoriteWord)
+router.post('/',favoriteWordController.createFavoriteWord)
+router.put('/',favoriteWordController.updateFavoriteWordRaiting)
+router.delete('/',favoriteWordController.deleteFavoriteWord)
+
+module.exports = router
