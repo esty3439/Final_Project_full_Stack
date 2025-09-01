47570a3660d14e50ba08fb8d485c41e3ad9d0077
esty3439 <esty0548535141@gmail.com>
2025-09-01 10:12:13 +0300

    add my word feature in backend

---
+++ backend/routes/myWordRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const router = express.Router()
+const myWordController = require('../controllers/myWordController')
+const verifyJWT = require('../middleware/verifyJWT')
+
+//use middleware
+router.use(verifyJWT)
+
+router.get('/',myWordController.getAllMyWords)
+router.post('/',myWordController.createMyWord)
+router.put('/',myWordController.updateMyWord)
+router.put('/rateing',myWordController.updateMyWordRaiting)
+router.delete('/',myWordController.deleteMyeWord)
+
+module.exports = router
